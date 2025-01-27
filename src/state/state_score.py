684232aa0b564856349313156f8fb97ec6c9acd4
Andrew Kula <arkula931@gmail.com>
2025-01-26 20:31:41 -0600

    added more to state_score

--- src/state/state_score.py
+++ src/state/state_score.py
@@ -1,4 +1,5 @@
 import game_state
+import math
 
 # This file retreives the current board score in relation to the bot (black)
 # It derives its score from the current piece values, the board position,
@@ -10,6 +11,8 @@ import game_state
 def get_board_score(state: str):
     gameState = game_state.GameState(state)
     currentScore = 0
+
+    print(gameState.get_board())
 
     # Call the three score calculators
     currentScore += piece_score(gameState.get_board_as_string())
@@ -40,13 +43,11 @@ def piece_score(pieces: list[str]):
     return runningTotal
 
 
-# Pieces = grid and castling_rights = short string
-def positional_score(pieces: list[str], castling_rights: str):
+def pieces_dict(pieces):
+    # Documents all the pieces for easy access to individual coordinates
 
-    totalScore = 0
-
-    # Runs through grid and documents all relevant positions
     piece_locs = {}
+
     for y in range(len(pieces)):
         for x in range(len(pieces[y])):
             if pieces[y][x] not in piece_locs:
@@ -54,27 +55,81 @@ def positional_score(pieces: list[str], castling_rights: str):
             else:
                 piece_locs[pieces[y][x]].append((x, y))
 
+    return piece_locs
+
+
+def positional_score(pieces: list[str], castling_rights: str):
+
+    score = 0
+    piece_locs = pieces_dict(pieces)
+
     # King safety
     kingX, kingY = piece_locs['k'][0]
 
     if 'k' in castling_rights or 'q' in castling_rights:
-        totalScore += 1
+        score += 1
 
-    if kingY == 0:
-        totalScore += 1
-    elif kingY > 1:
-        totalScore -= 1
+    if (kingX == 3 or kingX == 4) and (kingY == 3 or kingY == 4):
+        score -= 1
 
-    # Checks nearby pawns
+    # Checks pawns nearby the king
     front_pawns = list(
-        filter(lambda x: x[0] >= kingX - 1 and x[0] <= kingX + 1 and x[1] == kingY + 1, piece_locs['p']))
+        filter(lambda x: kingX - 1 <= x[0] <= kingX + 1 and x[1] == kingY + 1, piece_locs['p']))
     side_pawns = list(
         filter(lambda x: (x[0] == kingX - 1 or x[0] == kingX + 1) and x[1] == kingY, piece_locs['p']))
-    totalScore += len(front_pawns) + len(side_pawns)
+    score += len(front_pawns) + len(side_pawns)
 
-    return totalScore
+    # Center control
+    center_corners = [pieces[3][3], pieces[3][4], pieces[4][3], pieces[4][4]]
+    for corner in center_corners:
+        if corner.islower():
+            score += 1
+        elif corner == 'p' or corner == 'q':
+            score += 1
+
+    # Moveability
+
+    return score
+
+
+def available_moves(pieces, type: str, position):
+    def blank_check(value):
+        nonlocal score
+        if value == '.':
+            score += .1
+
+    score = 0
+    x, y = position
+
+    # King movement
+    if type == 'k':
+        for dy in range(-1, 2, 1):
+            for dx in range(-1, 2, 1):
+                if y + dy < 0 or x + dx < 0:
+                    continue
+                blank_check(pieces[y + dy][x + dx])
+
+    # Rook movement
+    if type == 'r':
+        for dy in range(8):
+            blank_check(pieces[dy][x])
+        for dx in range(8):
+            blank_check(pieces[y][dx])
+
+    return score
+
+
+'''
+
+All below if for debugging and should be deleted after completion
+
+'''
 
 
 state = game_state.GameState(
     "rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1")
-positional_score(state.get_board(), 'KQkq')
+# positional_score(state.get_board(), 'KQkq')
+print(available_moves(state.get_board(), 'k', (4, 0)))
+
+test_state = "rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1"
+# get_board_score(test_state)
